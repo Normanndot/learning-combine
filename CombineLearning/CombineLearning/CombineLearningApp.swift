2ec3620cb0eb9f5995564022a631d2503a5060de
Norman <normanndot@gmail.com>
2022-11-04 09:55:37 +0000

    Initial commit

---
+++ CombineLearning/CombineLearning/CombineLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineLearningApp.swift
+//  CombineLearning
+//
+//  Created by Norman D on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineLearning/CombineLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineLearning
+//
+//  Created by Norman D on 04/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
